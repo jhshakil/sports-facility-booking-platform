c4e484753248e7867e2c8ac9f4ef83f829216946
jhshakil <shakil11271@gmail.com>
2024-06-14 01:22:28 +0600

    setup project

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
+import router from './app/routes';
+
+const app: Application = express();
+
+// parsers
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: ['http://localhost:5173'] }));
+
+// application routes
+app.use('/api/v1', router);
+
+app.get('/', (req: Request, res: Response) => {
+  const name = 'Hello World!';
+  res.send(name);
+});
+
+app.use(globalErrorHandler);
+app.use(notFound);
+
+export default app;
