a041ebd408416c3c4e02d508493eb9034199942e
jhshakil <shakil11271@gmail.com>
2024-06-21 22:19:26 +0600

    deploy in vercel

--- dist/app.js
+++ dist/app.js
@@ -4,11 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const globalErrorHandler_1 = __importDefault(require("./app/middlewares/globalErrorHandler"));
+const notFound_1 = __importDefault(require("./app/middlewares/notFound"));
+const routes_1 = __importDefault(require("./app/routes"));
 const app = (0, express_1.default)();
-const port = 3000;
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+// parsers
+app.use(express_1.default.json());
+app.use((0, cookie_parser_1.default)());
+app.use((0, cors_1.default)({ origin: ['http://localhost:5173'] }));
+// application routes
+app.use('/api', routes_1.default);
+app.get('/', (req, res) => {
+    const name = 'Hello World!';
+    res.send(name);
 });
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+app.use(globalErrorHandler_1.default);
+app.use(notFound_1.default);
+exports.default = app;
